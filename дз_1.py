fef1b8e8e67eadbad6142722f43e4a9258d3e7de
katevzadze <143904679+katevzadze@users.noreply.github.com>
2023-10-26 20:57:45 +0300

    Update дз_1.py

--- дз_1.py
+++ дз_1.py
@@ -1,1 +1,26 @@
+from typing import Literal
 
+
+class Animal:
+    def __init__(
+            self,
+            animal_type: str,
+            animal_talk: str,
+            animal_eat: Literal["мясо", "рыбу", "траву"]):
+        self.animal_type = animal_type
+        self.animal_talk = animal_talk
+        self.animal_eat = animal_eat
+
+    def __str__(self) -> str:
+        return f"Это {self.animal_type}."
+
+    def action(self) -> str:
+        return f"{self.animal_type} издает звук {self.animal_talk} и ест {self.animal_eat}."
+
+
+tiger = Animal("Тигр", "ррр", "мясо")
+cat = Animal("Кот", "мяу", "рыбу")
+caw = Animal("Корова", "му", "траву")
+print(tiger, tiger.action())
+print(cat, cat.action())
+print(caw, caw.action())
